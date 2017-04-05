b54c658a7a862ada720898b88386e0efa6683aef
rodrigo-delgado <rod.delgado85@gmail.com>
2017-04-06 10:38:58 +1200

    add route for single todo

--- routes/todos.js
+++ routes/todos.js
@@ -3,6 +3,7 @@ var router = express.Router()
 var mongojs = require('mongojs')
 var db = mongojs ('mongodb://gotyou:123123123@ds153730.mlab.com:53730/meantodos',['todos'])
 
+//Get all todos
 router.get('/todos', function(req, res, next) {
   db.todos.find(function(err, todos) {
     if(err) {
@@ -12,5 +13,19 @@ router.get('/todos', function(req, res, next) {
     }
   })
 })
+
+//Get single todo
+router.get('/todo/:id', function(req, res, next) {
+  db.todos.findOne({
+    _id:mongojs.ObjectId(req.params.id)
+  },function(err, todo) {
+    if(err) {
+      res.send(err)
+    } else {
+      res.json(todo)
+    }
+  })
+})
+
 
 module.exports = router
