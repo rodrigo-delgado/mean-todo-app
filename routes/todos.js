3a2f427b7c37dc91ed86c5a8ef92be3280ba9844
rodrigo-delgado <rod.delgado85@gmail.com>
2017-04-06 10:54:38 +1200

    add delete route

--- routes/todos.js
+++ routes/todos.js
@@ -27,5 +27,72 @@ router.get('/todo/:id', function(req, res, next) {
   })
 })
 
+// Save todo
+
+router.post('/todo', function(req, res, next) {
+  var todo = req.body
+  if(!todo.text || !(todo.isCompleted + '')) {
+    res.status(400)
+    res.json({
+      "error": "Invalid Data"
+    })
+
+  } else {
+      db.save(todo, function(err, result) {
+        if(err) {
+          res.send(err)
+        } else {
+          res.json(result)
+        }
+      })
+    }
+})
+
+//Update Todo
+router.put('/todo/:id', function(req, res, next) {
+  var todo = req.body
+  var updateObj = {}
+
+  if(todo.isCompleted) {
+    updateObj.isCompleted = todo.isCompleted
+  }
+
+  if(todo.text) {
+    updateObj.text = todo.text
+  }
+
+  if(!updateObj) {
+    res.status(400)
+    res.json({
+      "error": "Invalid Data"
+    })
+  } else {
+    db.todo.update({
+      _id: mongojs.ObjectId(req.params.id)
+    }, updateObj, {}, function(err, result) {
+        if(err) {
+          res.send(err)
+        } else {
+          res.json(result)
+        }
+    })
+  }
+})
+
+//Delete todo
+
+router.delete('/todo/:id', function(req, res, next) {
+
+  db.todo.remove({
+    _id: mongojs.ObjectId(req.params.id)
+  }, '', function(err, result) {
+      if(err) {
+        res.send(err)
+      } else {
+        res.json(result)
+        }
+  })
+})
+
 
 module.exports = router
