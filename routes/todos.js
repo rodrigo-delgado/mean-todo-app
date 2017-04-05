3a1911284133c8ca59c194ac2cd1cf5d6b2c2dfd
rodrigo-delgado <rod.delgado85@gmail.com>
2017-04-06 10:23:34 +1200

    add mlab todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var router = express.Router()
+var mongojs = require('mongojs')
+var db = mongojs ('mongodb://gotyou:123123123@ds153730.mlab.com:53730/meantodos',['todos'])
 
 router.get('/todos', function(req, res, next) {
-  res.send('TODOS API')
+  db.todos.find(function(err, todos) {
+    if(err) {
+      res.send(err)
+    } else {
+      res.json(todos)
+    }
+  })
 })
 
 module.exports = router
